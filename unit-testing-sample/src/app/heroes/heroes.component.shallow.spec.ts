081dfaa97987d973c5ea55a227b53f41b0409d5d
Joshi Bhargav <bJoshi3@anglianwater.co.uk>
2019-08-30 07:09:00 +0100

    Shallow integration tests added

---
+++ unit-testing-sample/src/app/heroes/heroes.component.shallow.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { HeroesComponent } from './heroes.component';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { Component, Input } from '@angular/core';
+import { of } from 'rxjs';
+
+describe('Heroes Component (Shallow Tests)', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
+
+  @Component({
+    selector: 'app-hero',
+    template: '<div></div>'
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+  }
+
+  beforeEach(() => {
+    HEROES = [
+      { id: 1, name: 'Spider Dude', strength: 8 },
+      { id: 2, name: 'Wonderful Woman', strength: 24 },
+      { id: 3, name: 'Super Dude', strength: 55 }
+    ];
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+    TestBed.configureTestingModule({
+      declarations: [HeroesComponent, FakeHeroComponent],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService }
+      ]
+    });
+    fixture = TestBed.createComponent(HeroesComponent);
+  });
+
+  it('should set heroes correctly from the service', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.heroes.length).toBe(3);
+  });
+
+
+
+});
